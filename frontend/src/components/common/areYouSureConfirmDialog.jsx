aaaeaa0f8a0549de2c855c7ee35c7b7d9d15533b
yolylight <yolylight@hotmail.com>
2024-04-26 10:24:48 +0800

    Update areYouSureConfirmDialog.jsx

--- frontend/src/components/common/areYouSureConfirmDialog.jsx
+++ frontend/src/components/common/areYouSureConfirmDialog.jsx
@@ -8,15 +8,15 @@ export const areYouSureConfirmDialog = (opName) =>
     title: (
       <div className="d-flex-start-center">
         <IoMdWarning className="text-warning" />
-        <span className="ms-1">Confirm</span>
+        <span className="ms-1">确认</span>
       </div>
     ),
     message: (
       <div className="text-wrap">
-        <h6 className="text-muted">Operation:</h6>
+        <h6 className="text-muted">操作:</h6>
         <h6 className="text-center text-ul fst-italic">{opName}</h6>
         <hr className="bg-dark" />
-        <span className="">Are you sure ?</span>
+        <span className="">你确定么 ?</span>
       </div>
     ),
     confirmColor: "secondary",

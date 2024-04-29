64a821eea1a2b0ef4e97e54ccf215feab3d6d5eb
yolylight <yolylight@hotmail.com>
2024-04-29 16:18:20 +0800

    Update OrgCreateButton.jsx

--- frontend/src/components/organization/utils/OrgCreateButton.jsx
+++ frontend/src/components/organization/utils/OrgCreateButton.jsx
@@ -19,11 +19,11 @@ const onValidate = (values) => {
   const maxLength = 32;
   const errors = {};
   if (!values.name) {
-    errors.name = "This field is required.";
+    errors.name = "该字段必填.";
   } else if (values.name.length < minLength) {
-    errors.name = `This field must be at least ${minLength} characters long`;
+    errors.name = `该字段最小 ${minLength} 个字符`;
   } else if (values.name.length >= maxLength) {
-    errors.name = `This field must be no more than ${maxLength} characters long.`;
+    errors.name = `该字段最大${maxLength} 个字符.`;
   }
   return errors;
 };
@@ -57,7 +57,7 @@ function OrganizationCreateForm({ onFormSubmit }) {
         <Form className="mx-2 my-3">
           <FormGroup row className="d-flex flex-wrap">
             <Label className="required" for="orgforminput-name" md={3}>
-              Organization Name
+              组织名称
             </Label>
             <Col md={6}>
               <Input
@@ -95,7 +95,7 @@ OrganizationCreateForm.propTypes = {
 function OrgCreateIcon() {
   return (
     <span>
-      <IoMdPersonAdd className="me-1" /> Create a new organization
+      <IoMdPersonAdd className="me-1" /> 创建新组织
     </span>
   );
 }

a12acc8f8b19213144888f7cf7ca43f1f1d802fc
yolylight <yolylight@hotmail.com>
2024-04-29 11:24:07 +0800

    Update notificationApi.js

--- frontend/src/components/jobs/notification/notificationApi.js
+++ frontend/src/components/jobs/notification/notificationApi.js
@@ -9,7 +9,7 @@ export async function notificationMarkAsRead(notifId) {
     await axios.post(`${NOTIFICATION_BASE_URI}/${notifId}/mark-as-read`);
     return Promise.resolve(true);
   } catch (error) {
-    addToast("Failed!", error.parsedMsg, "danger", true);
+    addToast("失败!", error.parsedMsg, "danger", true);
     return Promise.reject(error);
   }
 }

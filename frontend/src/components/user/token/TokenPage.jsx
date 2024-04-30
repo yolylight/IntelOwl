8d5f8d307771bb86fbd04f8922267ec8d8f67a23
yolylight <yolylight@hotmail.com>
2024-04-30 15:35:21 +0800

    Update TokenPage.jsx

--- frontend/src/components/user/token/TokenPage.jsx
+++ frontend/src/components/user/token/TokenPage.jsx
@@ -21,22 +21,22 @@ export default function TokenPage() {
       <Row className="my-4">
         <Alert color="secondary" className="mx-3 mx-md-auto text-center">
           <span>
-            You can generate an API key to access IntelOwl&apos;s RESTful API.
-            Take a look to the available Python and Go clients:
+            您可以生成一个 API 密钥来访问 IntelOwl 的 RESTful API.
+            查看可用的 Python 和 Go 客户端:
             <a
               href={PYINTELOWL_GH_URL}
               target="_blank"
               rel="noreferrer"
               className="link-primary"
             >
-              Learn more
+              了解更多
             </a>
             .
           </span>
         </Alert>
       </Row>
       {/* API Access */}
-      <h6>API Access</h6>
+      <h6>API 访问</h6>
       <ContentSection className="bg-body border border-dark">
         <TokenAccess />
       </ContentSection>
